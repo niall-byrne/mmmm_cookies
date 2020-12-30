7305fcf235de7e56588186b3384edbd2be21f6e6
GitHub Action <action@github.com>
2020-12-30 14:46:57 +0000

    Cookiecutter: Initial Generation

---
+++ scripts/settings.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+PROJECT_HOME="$(git rev-parse --show-toplevel)"
+PROJECT_NAME="mmmm_cookies"
+PROJECT_AUTHOR="Niall Byrne"
+
+export PROJECT_HOME
+export PROJECT_NAME
+export PROJECT_AUTHOR
